7731e55f8e2ac1c3e7918f130dad9ef6dc95c20f
Shyam-AI <shyamdl2803@gmail.com>
2021-10-30 20:24:10 +0530

    updated requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+## edit below variables as per your requirements -
+REPO_NAME = "dvc-nlp"   #https://github.com/Shyam-AI/dvc-nlp
+AUTHOR_USER_NAME = "Shyam-AI"
+SRC_REPO = "src"
+LIST_OF_REQUIREMENTS = []
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A package for DVC NLP usecase",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Shyam-AI/dvc-nlp",
+    author_email="shyamdl2803@gmail.com",
+    packages=[SRC_REPO],
+    license="MIT",
+    python_requires=">=3.6",
+    install_requires=LIST_OF_REQUIREMENTS
+)

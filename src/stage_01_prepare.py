5db2e96ed7c35c1a14bf0c485e541a14919de866
Shyam-AI <shyamdl2803@gmail.com>
2021-10-31 11:35:46 +0530

    added stage-01 data prep

---
+++ src/stage_01_prepare.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+
+
+STAGE = "one"
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+def main(config_path):
+    pass
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<n")
+    except Exception as e:
+        logging.exception(e)
+        raise e

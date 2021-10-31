ba46187139243050a7ae77ad40c7718ead087846
Shyam-AI <shyamdl2803@gmail.com>
2021-10-31 11:47:29 +0530

    added yaml load for stage-01

--- src/stage_01_prepare.py
+++ src/stage_01_prepare.py
@@ -3,9 +3,11 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
+from src.utils.common import read_yaml
 
 
 STAGE = "one"
+
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
     level=logging.INFO, 
@@ -13,18 +15,20 @@ logging.basicConfig(
     filemode="a"
     )
 
-def main(config_path):
-    pass
+def main(config_path, params_path):
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
-        main(config_path=parsed_args.config)
+        main(config_path=parsed_args.config, params_path = parsed_args.params)
         logging.info(f">>>>> stage {STAGE} completed!<<<<<n")
     except Exception as e:
         logging.exception(e)

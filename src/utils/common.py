6bc03587f7ec44f9da5de903895e6c8fb1e59848
Shyam-AI <shyamdl2803@gmail.com>
2021-11-13 20:12:32 +0530

    completed stage-02

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,6 +1,8 @@
-
 import yaml
 import os 
+import logging
+import pandas as pd
+
 
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
@@ -10,9 +12,20 @@ def read_yaml(path_to_yaml: str) -> dict:
 def create_directory(dirs: list):
     for dir_path in dirs:
         os.makedirs(dir_path, exist_ok=True)
-        print(f"the directory is created at {dir_path}")
-    
+        #print(f"the directory is created at {dir_path}")
+        logging.info(f"the directory is created at {dir_path}")
 
 def save_local_df(data, data_path, index=False):
     data.to_csv(data_path, index=index)
     print(f"data frame save at {data_path}")
+
+def get_df(path_to_data: str, sep: str="\t") -> pd.DataFrame:
+    df = pd.read_csv(
+        path_to_data,
+        encoding="utf-8",
+        header=None,
+        delimiter=sep,
+        names=["id", "label", "text"],
+    )
+    logging.info(f"The input data frame {path_to_data} size is {df.shape}\n")
+    return df

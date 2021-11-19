4e5c977128765c77c7f9c01e9bbf6192c3a91011
Shyam-AI <shyamdl2803@gmail.com>
2021-11-19 18:47:51 +0530

    stage_04 initialized

---
+++ src/stage_04_evaluate.py
@@ -0,0 +1,39 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+import joblib
+import numpy as np
+import sklearn.metrics as metrics
+import math
+
+
+STAGE = "Four"
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+
+def main(config_path):
+    pass
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
